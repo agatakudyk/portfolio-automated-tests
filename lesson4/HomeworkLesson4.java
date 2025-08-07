d59428352128ccd73fa31f3183c9adcef8c39cc1
Agata Kudyk <agata.kudyk@gmail.com>
2025-08-07 16:34:28 +0200

    Modyfikacja nazw metod i wywołań

--- lesson4/HomeworkLesson4.java
+++ lesson4/HomeworkLesson4.java
@@ -1,27 +1,29 @@
+package lesson4.homework;
+
 public class HomeworkLesson4 {
     public static void main(String[] args) {
         // Zadanie 1 - obliczenie oceny
-        zad1Ocena(93);
-        zad1Ocena(87);
-        zad1Ocena(71);
-        zad1Ocena(65);
-        zad1Ocena(22);
-        zad1Ocena(222);
+        showGrade(93);
+        showGrade(87);
+        showGrade(71);
+        showGrade(65);
+        showGrade(22);
+        showGrade(222);
         // Zadanie 3 - liczba parzysta i nieparzysta
-        zad3Parzysta(20);
-        zad3Parzysta(34);
-        zad3Parzysta(15);
-        zad3Parzysta(19);
-        zad3Parzysta(-10);
-        zad3Parzysta(-9);
+        checkNumber(20);
+        checkNumber(34);
+        checkNumber(15);
+        checkNumber(19);
+        checkNumber(-10);
+        checkNumber(-9);
         // Zadanie 2 - konwerter temperatury
-        zad2Temperatura(40, 'C');
-        zad2Temperatura(-10, 'C');
-        zad2Temperatura(100, 'F');
-        zad2Temperatura(0, 'F');
+        showTemperature(40, 'C');
+        showTemperature(-10, 'C');
+        showTemperature(100, 'F');
+        showTemperature(0, 'F');
     }
 
-    public static void zad1Ocena(int number) {
+    public static void showGrade(int number) {
 
         if (number <= 100 && number >= 90) {
             System.out.println("Ocena A");
@@ -38,7 +40,7 @@ public class HomeworkLesson4 {
         }
     }
 
-    public static void zad3Parzysta(int liczba) {
+    public static void checkNumber(int liczba) {
 
         if (liczba % 2 == 0) {
             if (liczba % 5 == 0) {
@@ -55,15 +57,16 @@ public class HomeworkLesson4 {
             }
         }
     }
-    public static void zad2Temperatura(int temperatura, char typ) {
 
-            if (typ == 'F') {
-                System.out.println((temperatura - 32) * 5/9 + "C");
-            } else if (typ == 'C') {
-                System.out.println((temperatura * 9/5) +32 + "F");
-            } else {
-                System.out.println("Nierozpoznany typ temperatury");
-            }
+    public static void showTemperature(int temperatura, char typ) {
+
+        if (typ == 'F') {
+            System.out.println((temperatura - 32) * 5 / 9 + "C");
+        } else if (typ == 'C') {
+            System.out.println((temperatura * 9 / 5) + 32 + "F");
+        } else {
+            System.out.println("Nierozpoznany typ temperatury");
+        }
     }
 }
 
--- lesson4/README.md
+++ lesson4/README.md
@@ -4,7 +4,7 @@
 To repozytorium służy do zadań i projektów realizowanych podczas kursu "Tester Automatyzujący & AI" w Future Collars. Jego celem jest dokumentowanie postępów w nauce.
 
 ## Narzędzia
-- język Java
-- IntelliJ
-- system kontroli wersji Git
-- platforma GitHub
+- język programowania Java,
+- program IntelliJ IDEA,
+- system kontroli wersji Git,
+- platforma GitHub.
