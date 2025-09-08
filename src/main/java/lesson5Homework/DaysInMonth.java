545d50c491836c7ae727c3d926a153ea281c961d
Agata Kudyk <agata.kudyk@gmail.com>
2025-09-08 14:50:04 +0200

    Lesson5Homework / utworzenie kodu do zadania 3 - kod zwracający ilość dni w miesiącu

---
+++ src/main/java/lesson5Homework/DaysInMonth.java
@@ -0,0 +1,22 @@
+package lesson5Homework;
+
+public class DaysInMonth {
+
+    public static void main(String[]args) {
+
+    }
+    public static int DaysInMonth() {
+
+        int monthNumber = 2;
+        int year = 2024;
+
+
+    }
+}
+
+
+//Miesiące z 31 dniami
+//Styczeń (1), Marzec (3), Maj (5), Lipiec (7), Sierpień (8), Październik (10), Grudzień (12).
+//Miesiące z 30 dniami Kwiecień (4), Czerwiec (6), Wrzesień (9), Listopad (11).
+//Luty
+//Luty ma 28 dni, a w roku przestępnym ma 29 dni.
