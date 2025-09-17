3ac3b0609990069f9aab1fc9b48f2296477134fd
Agata Kudyk <agata.kudyk@gmail.com>
2025-09-17 14:20:11 +0200

    HomeworkLesson8 - dokończenie kodu w pliku AcceptNewsLetterTest

--- src/test/java/lesson8Homework/AcceptNewsLetterTest.java
+++ src/test/java/lesson8Homework/AcceptNewsLetterTest.java
@@ -16,13 +16,13 @@ import java.time.Duration;
 public class AcceptNewsLetterTest {
 
     private WebDriver driver;
-    private WebDriverWait wait;
+    private WebDriverWait wait;   //czekanie aż strona się załaduje
 
     @BeforeEach
-    void setup() {
+    public void setup() {
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
-        driver.manage().window().maximize();
+        driver.manage().window().maximize();  //powiększenie okna
         wait = new WebDriverWait(driver, Duration.ofSeconds(10));
     }
 
@@ -40,7 +40,7 @@ public class AcceptNewsLetterTest {
 
         driver.findElement(By.xpath("//input[@placeholder='Your email address']"))
                 .sendKeys("test142@wp.pl");
-        wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector(".submitNewsletter"))).click();
+        driver.findElement(By.xpath("//input[@name='submitNewsletter']")).click();
 
         String newsletterMessage = wait.until(
                 ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".alert-success"))
@@ -51,7 +51,7 @@ public class AcceptNewsLetterTest {
     }
 
     @AfterEach
-    void teardown() {
+    public void teardown() {
         if (driver != null) {
             driver.quit();
         }
