fa36de2e00ee76d4fef211e6a9d325f8635ac003
Agata Kudyk <agata.kudyk@gmail.com>
2025-09-17 12:31:08 +0200

    HomeworkLesson8 - rozwinięcie kodu w pliku AcceptNewsLetterTest

---
+++ src/test/java/lesson8Homework/AcceptNewsLetterTest.java
@@ -0,0 +1,59 @@
+package lesson8Homework;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class AcceptNewsLetterTest {
+
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    @BeforeEach
+    void setup() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+    }
+
+    @Test
+    public void Prestashop() {
+
+        driver.get("https://demo.prestashop.com/#/en/front");
+
+
+        wait.until(ExpectedConditions.frameToBeAvailableAndSwitchToIt(By.cssSelector("iframe[id^='framelive']")));
+
+
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@placeholder='Your email address']")));
+
+
+        driver.findElement(By.xpath("//input[@placeholder='Your email address']"))
+                .sendKeys("test142@wp.pl");
+        wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector(".submitNewsletter"))).click();
+
+        String newsletterMessage = wait.until(
+                ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".alert-success"))
+        ).getText();
+
+
+        Assertions.assertEquals("You have successfully subscribed to this newsletter.", newsletterMessage);
+    }
+
+    @AfterEach
+    void teardown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}
