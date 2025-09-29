5ad55c68141f1e9460e075c720cdc8c98a6580ac
Agata Kudyk <agata.kudyk@gmail.com>
2025-09-29 11:46:08 +0200

    Lesson12 / test sklepu Presta Shop - dodanie metody 'checkFooterLinksClickable'

--- src/test/java/lesson12Homework/SeleniumPrestashopTest.java
+++ src/test/java/lesson12Homework/SeleniumPrestashopTest.java
@@ -13,7 +13,7 @@ import java.time.Duration;
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class SeleniumPrestashopTest {
 
-    static ChromeDriver driver;  // sterownik przeglądarki
+    static ChromeDriver driver;
     static WebDriverWait wait;
 
     @BeforeAll
@@ -23,8 +23,123 @@ public class SeleniumPrestashopTest {
         driver.get("http://localhost:8080/pl/");
     }
 
-    @Test   //zmiana języka z polskiego na angielski
+    @Test   //Strona główna/Footer -  sprawdzenie działania linków w stopce
     @Order(1)
+    public void checkFooterLinksClickable() {
+
+        //Kliknięcie w link 'Prices drop'
+        By pricesDropLinkLocator = By.id("link-product-page-prices-drop-1");
+        WebElement pricesDropLink = driver.findElement(pricesDropLinkLocator);
+        pricesDropLink.click();
+        //potwierdzenie otwarcia podstrony
+        By pricesDropPageNameLocator = By.id("js-product-list-header");
+        WebElement pricesDropPageName = driver.findElement(pricesDropPageNameLocator);
+        Assertions.assertTrue(pricesDropPageName.isDisplayed());
+
+        //Kliknięcie w link 'New products'
+        By newProductsLinkLocator = By.id("link-product-page-new-products-1");
+        WebElement newProductsLink = driver.findElement(newProductsLinkLocator);
+        newProductsLink.click();
+        //potwierdzenie wejścia na podstronę
+        By newProductsPageNameLocator = By.id("js-product-list-header");
+        WebElement newProductsPageName = driver.findElement(newProductsPageNameLocator);
+        Assertions.assertTrue(newProductsPageName.isDisplayed());
+
+        //Kliknięcie w link 'Best sales'
+        By bestSalesLinkLocator = By.id("link-product-page-best-sales-1");
+        WebElement bestSalesLink = driver.findElement(bestSalesLinkLocator);
+        bestSalesLink.click();
+        //potwierdzenie wejścia na podstronę
+        By bestSellersPageNameLocator = By.id("js-product-list-header");
+        WebElement bestSellersPageName = driver.findElement(bestSellersPageNameLocator);
+        Assertions.assertTrue(bestSellersPageName.isDisplayed());
+
+        //Kliknięcie w link 'Delivery'
+        By deliveryLinkLocator = By.id("link-cms-page-1-2");
+        WebElement deliveryLink = driver.findElement(deliveryLinkLocator);
+        deliveryLink.click();
+        //ToDo potwierdzenie wejścia na podstronę
+
+        //Kliknięcie w link 'Legal Notice'
+        By legalNoticeLinkLocator = By.id("link-cms-page-2-2");
+        WebElement legalNoticeLink = driver.findElement(legalNoticeLinkLocator);
+        legalNoticeLink.click();
+        //ToDo potwierdzenie wejścia na podstronę
+
+        //Kliknięcie w link 'Terms and conditions of use'
+        By termsAndConditionsOfUseLinkLocator = By.id("link-cms-page-3-2");
+        WebElement termsAndConditionsOfUseLink = driver.findElement(termsAndConditionsOfUseLinkLocator);
+        termsAndConditionsOfUseLink.click();
+        //ToDo potwierdzenie wejścia na podstronę
+
+        //Kliknięcie w link 'About us'
+        By aboutUsLinkLocator = By.id("link-cms-page-4-2");
+        WebElement aboutUsLink = driver.findElement(aboutUsLinkLocator);
+        aboutUsLink.click();
+        //ToDo potwierdzenie wejścia na podstronę
+
+        //Kliknięcie w link 'Secure payment'
+        By securePaymentLinkLocator = By.id("link-cms-page-5-2");
+        WebElement securePaymentLink = driver.findElement(securePaymentLinkLocator);
+        securePaymentLink.click();
+        //ToDo potwierdzenie wejścia na podstronę
+
+        //Kliknięcie w link 'Contact us'
+        By contactUsLinkLocator = By.id("link-static-page-contact-2");
+        WebElement contactUsLink = driver.findElement(contactUsLinkLocator);
+        contactUsLink.click();
+        //ToDo potwierdzenie wejścia na podstronę
+
+        //Kliknięcie w link 'Sitemap'
+        By sitemapLinkLocator = By.id("link-static-page-sitemap-2");
+        WebElement sitemapLink = driver.findElement(sitemapLinkLocator);
+        sitemapLink.click();
+        //ToDo potwierdzenie wejścia na podstronę
+
+        //Kliknięcie w link 'Stores'
+        By storesLinkLocator = By.id("link-static-page-stores-2");
+        WebElement storesLink = driver.findElement(storesLinkLocator);
+        storesLink.click();
+        //potwierdzenia wejscie na podstronę
+        By ourStoresPageNameLocator = By.xpath("//header[@class=\"page-header\"]/h1");
+        WebElement ourStoresPageName = driver.findElement();
+        Assertions.assertTrue(ourStoresPageName.isDisplayed());
+
+        //Kliknięcie w link 'Personal info'
+        By personalInfoLinkLocator = By.xpath("//a[@title=\"Personal info\"]");
+        WebElement personalInfoLink = driver.findElement(personalInfoLinkLocator);
+        personalInfoLink.click();
+        //ToDo potwierdzenie wejścia na podstronę
+
+        //Kliknięcie w link 'Orders'
+        By ordersLinkLocator = By.xpath("//a[@title=\"Orders\"]");
+        WebElement ordersLink = driver.findElement(ordersLinkLocator);
+        ordersLink.click();
+        //ToDo potwierdzenie wejścia na podstronę
+
+        //Kliknięcie w link 'Credit slips'
+        By creditSlipsLinkLocator = By.xpath("//a[@title=\"Credit slips\"]");
+        WebElement creditSlipsLink = driver.findElement(creditSlipsLinkLocator);
+        creditSlipsLink.click();
+        //ToDo potwierdzenie wejścia na podstronę
+
+        //Kliknięcie w link 'Addresses'
+        By addressesLinkLocator = By.xpath("//a[@title=\"Addresses\"]");
+        WebElement addressesLink = driver.findElement(addressesLinkLocator);
+        addressesLink.click();
+        //ToDo potwierdzenie wejścia na podstronę
+
+        //Kliknięcie w maila kontaktowego
+        By mailLinkLocator = By.xpath("//a[contains(text(),\"prestashop@123.pl\")]");
+        WebElement mailLink = driver.findElement(mailLinkLocator);
+        mailLink.click();
+        //ToDo potwierdzenie wejścia na podstronę
+    }
+
+
+
+    @Test   //zmiana języka z polskiego na angielski
+    @Order(2)
     public void languageSwitchIntoEnglish() {
 
         By languageSwitchLocator = By.xpath("//button[@data-toggle=\"dropdown\"]");
@@ -39,8 +154,8 @@ public class SeleniumPrestashopTest {
 
 
     //@Test  //Rejestracja: kliknięcie ‘Save’ przy pustym formularzu + walidacja
-    @Order(2)
-    public void failSignupWithEmptyFieldsTest() {
+    @Order(3)
+    public void failSignupWithEmptyFields() {
 
         //kliknięcie w przycisk logowania
         By signInLocator = By.cssSelector(".user-info a");
@@ -69,7 +184,7 @@ public class SeleniumPrestashopTest {
 
     //@Test  //Rejestracja: uzupełnienie formularza poprawnymi danymi + walidacja
     @Order(3)
-    public void userSuccessSignupTest() {
+    public void userSuccessSignup() {
 
         //uzupełnienie pola imię
         By nameLocator = By.id("field-firstname");
@@ -89,7 +204,7 @@ public class SeleniumPrestashopTest {
         //uzupełnienie pola hasło
         By passwordLocator = By.id("field-password");
         WebElement passwordInputField = driver.findElement(passwordLocator);
-        passwordInputField.sendKeys("password123");
+        passwordInputField.sendKeys("Testowo123");
 
         //checkbox informacji o przetwarzaniu danych osobowych
         By policyInfoLocator = By.xpath("//input[@name=\"customer_privacy\"]");
@@ -181,7 +296,7 @@ public class SeleniumPrestashopTest {
 
     //@Test     //Poprawne zalogowanie + walidacja
     @Order(7)
-    public void userSuccesLogin() {
+    public void userSuccessLogin() {
 
         //przejście na stronę logowania
         By backToLoginPageLocator = By.xpath("//i[@class=\"material-icons\"]");
@@ -236,7 +351,7 @@ public class SeleniumPrestashopTest {
         //TODO: potwiedzenie wyczyszczenia filtrów
     }
 
-    @Test    //Podstrona ART/filtrowanie: Black + walidacja
+   // @Test    //Podstrona ART/filtrowanie: Black + walidacja
     @Order(9)
     public void filterArtProducts() {
 
@@ -254,7 +369,7 @@ public class SeleniumPrestashopTest {
     }
 
 
-    @Test   //Wybranie produktu ‘The best is yet to come' + dodanie opinii + walidacja
+    //@Test   //Wybranie produktu ‘The best is yet to come' + dodanie opinii + walidacja
     @Order(10)
     public void successAddPosterReview() {
 
@@ -296,7 +411,7 @@ public class SeleniumPrestashopTest {
         okCommentButtonClick.click();
     }
 
-    @Test    //Dodanie 3x produktu do koszyka  + walidacja
+    //@Test    //Dodanie 3x produktu do koszyka  + walidacja
     @Order(11)
     public void addProductsToCart() {
 
@@ -318,7 +433,7 @@ public class SeleniumPrestashopTest {
         Assertions.assertTrue(addProductPopup.isDisplayed());
     }
 
-    @Test  //Koszyk + walidacja ilości produktów i ceny całkowitejV
+   // @Test  //Koszyk + walidacja ilości produktów i ceny całkowitejV
     @Order(12)
     public void cartContentValidation() {
 
@@ -338,7 +453,7 @@ public class SeleniumPrestashopTest {
         WebElement numberInItemSection = driver.findElement(numberInItemsSectionLocator);
         //pobranie ilości z sekcji podsumowania
         By numberInPurchaseSummarySection = By.xpath("//span[@class=\"label js-subtotal\"]");
-        Assertions.assertEquals(numberInItemSection,numberInPurchaseSummarySection);
+        Assertions.assertEquals(numberInItemSection, numberInPurchaseSummarySection);
 
         //sprawdzenie wartości całkowitej
         //pobranie wartości całkowitej
@@ -348,25 +463,73 @@ public class SeleniumPrestashopTest {
         By itemUnitPriceLocator = By.xpath("//span[@class=\"price\"]");
         WebElement itemUnitPrice = driver.findElement(itemUnitPriceLocator);
         //obliczenie
-         double expectedTotalPrice = numberInItemSection * itemUnitPrice;
+        //double expectedTotalPrice = numberInItemSection * itemUnitPrice;
         //sprawdzenie wartości całkowitej
-        Assertions.assertEquals(expectedTotalPrice,totalItemsValue);
+        //Assertions.assertEquals(expectedTotalPrice,totalItemsValue);
     }
 
-        @Test   //Koszyk + zwiększenie ilości + walidacja ilości produktów i ceny całkowitej
-        @Order(13)
-        public void increasingNumberOfItems() {
+    //@Test   //Koszyk + zwiększenie ilości + walidacja ilości produktów i ceny całkowitej
+    @Order(13)
+    public void increasingNumberOfItems() {
 
         //zwiększenie liczby produktu w koszyku
-            By selectQuantityLocator = By.xpath("//i[@class=\"material-icons touchspin-up\"]");
-            WebElement selectQuantityClick = driver.findElement(selectQuantityLocator);
-            selectQuantityClick.click();
-            selectQuantityClick.click();
-            selectQuantityClick.click();
+        By selectQuantityLocator = By.xpath("//i[@class=\"material-icons touchspin-up\"]");
+        WebElement selectQuantityClick = driver.findElement(selectQuantityLocator);
+        selectQuantityClick.click();
+        selectQuantityClick.click();
+        selectQuantityClick.click();
+    }
 
+    //@Test    //Formularz adresu – zapisanie pustego formularza + walidacja
+    @Order(14)
+    public void addressFormFailSendWithEmptyFields() {
 
+    }
 
-        }
+   // @Test    //Formularz adresu – zapisanie poprawnie uzupełnionego formularza
+    @Order(15)
+    public void addressFormSuccessSend() {
+
+    }
+
+//    @Test       //Wybór dostawy ‘My carrier’ + dodanie komentarza
+    @Order(16)
+    public void selectDeliveryMethod() {
+
+    }
+
+//    @Test    //wybór formy płatności + walidacja
+    @Order(17)
+    public void selectPaymentOption() {
+
+    }
+
+//    @Test    //Dokończenie zamówienia
+    @Order(18)
+    public void successProceedCheckout() {
+
+    }
+
+//    @Test   //Order history + sprawdzenie czy jest dokonane zamówienie
+    @Order(19)
+    public void viewItemsInOrderHistory() {
+
+    }
+
+    //@Test    //Order history + sprawdzenie szczegółów zamówienia
+    @Order(20)
+    public void viewOrderDetailsInOrderHistory() {
+
+    }
+
+    //@Test    //Order history + dodanie wiadomości
+    @Order(21)
+    public void addOrderMessageInOrderHistory() {
+
+    }
+
+    //@Test     //Reorder – edycja adresu + walidacja
+   // @Order(22)
 
     //@AfterAll
     public static void afterAll() {
