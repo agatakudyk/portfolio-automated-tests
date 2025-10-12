69adaf2f75bab581d56009ac24090ca9ac728dd3
Agata Kudyk <agata.kudyk@gmail.com>
2025-10-12 19:47:59 +0200

    Lesson15 / test sklepu 'PrestaShop' / folder 'pages' - poprawienie błędów w kodzie oraz zmiana nazwy pliku "Home.java"

---
+++ src/test/java/lesson15Homework/pages/Home.java
@@ -0,0 +1,44 @@
+package lesson15Homework.pages;
+
+import com.codeborne.selenide.selector.ByDeepShadowCss;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import static lesson15Homework.driver.DriverProvider.getDriver;
+
+public class Home {
+
+    //zmiana języka / kliknięcie w dropdown-button
+    public void languageDropdownButton() {
+        By languageDropdownButtonLocator = By.xpath("//button[@data-toggle=\"dropdown\"]");
+        getDriver().findElement(languageDropdownButtonLocator).click();
+    }
+
+    //wybór opcji 'English' na liście języków
+    public void englishLanguageSelectionFromDropdown() {
+        By englishLanguageSwitchLocator = By.xpath("//a[@data-iso-code=\"en\"]");
+        getDriver().findElement(englishLanguageSwitchLocator).click();
+    }
+
+    //kliknięcie wishlist-button produktu 'Today is a good day Framed Poster'
+    public void wishlistIconOfPosterTodayIsAGoodDay() {
+        By heartButtonOfTodayIsAGoodDayFramedPosterLocator = By.xpath(
+                "//a[contains(text(),\"Today is a good day Framed\")]" +
+                        "/../../../button[@class=\"wishlist-button-add\"]");
+        getDriver().findElement(heartButtonOfTodayIsAGoodDayFramedPosterLocator).click();
+    }
+
+    //kliknięcie w button 'Cancel' w Popupie
+    public void closeWishlistPopupOfUnregisteredUser() {
+        By cancelPopupButtonLocator = By.xpath("//a[contains(text(),\"Sign in\")]" +
+                "/../button[contains(text(),\"Cancel\")]");
+        getDriver().findElement(cancelPopupButtonLocator).click();
+    }
+
+    //wejście na stronę produktu 'Today is a good day Framed Poster'
+    public void enterProductPageOfTodayIsAGoodDayFramedPoster() {
+        By openTodayIsAGoodDayFramedPosterLocator = By.xpath("//a[@class=\"thumbnail product-thumbnail\"]" +
+                "/img[@alt=\"Today is a good day Framed poster\"]");
+        getDriver().findElement(openTodayIsAGoodDayFramedPosterLocator).click();
+    }
+}
