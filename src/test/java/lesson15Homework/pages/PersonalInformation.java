32a0908b414775135dbef94f276c9e57b09b7029
Agata Kudyk <agata.kudyk@gmail.com>
2025-10-13 13:14:58 +0200

    Lesson15 / test sklepu 'PrestaShop' / folder 'pages' - utworzenie pliku "PersonalInformation.java"

---
+++ src/test/java/lesson15Homework/pages/PersonalInformation.java
@@ -0,0 +1,28 @@
+package lesson15Homework.pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import static lesson15Homework.driver.DriverProvider.getDriver;
+
+
+public class PersonalInformation {
+
+    //Personal Information - checkbox zgody na przetwarzanie danych osobowych
+    public void customerPrivacyCheckbox() {
+        By policyInfoLocator = By.xpath("//input[@name=\"customer_privacy\"]");
+        getDriver().findElement(policyInfoLocator).click();
+    }
+
+    //Personal Information - checkbox akceptacji regulaminu i polityki prywatności
+    public void termsAndCinditionsCheckbox() {
+        By privacyPolicyLocator = By.xpath("//input[@name=\"psgdpr\"]");
+        getDriver().findElement(privacyPolicyLocator).click();
+    }
+
+    public void continueButton() {
+        By continueButtonLocator = By.xpath("//section[@id=\"checkout-personal-information-step\"]" +
+                "//button[@type=\"submit\"]");
+        getDriver().findElement(continueButtonLocator).click();
+    }
+}
