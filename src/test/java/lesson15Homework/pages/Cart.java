46fdf84ab317a01f65d5369c0e4a4fb9f3dbe807
Agata Kudyk <agata.kudyk@gmail.com>
2025-10-13 11:38:01 +0200

    Lesson15 / test sklepu 'PrestaShop' / folder 'pages' - modyfikacja kodu w pliku "Cart.java"

--- src/test/java/lesson15Homework/pages/Cart.java
+++ src/test/java/lesson15Homework/pages/Cart.java
@@ -1,20 +1,26 @@
 package lesson15Homework.pages;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
-
 import static lesson15Homework.driver.DriverProvider.getDriver;
 
 
 public class Cart {
 
-    //Shopping cart - usunięcie produktu z koszyka
+    //usunięcie produktu z koszyka
     public void deleteCartItem() {
         By trashIconLocator = By.xpath("//a[@class=\"remove-from-cart\"]/i[contains(text(),\"delete\")]");
         getDriver().findElement(trashIconLocator).click();
     }
 
+        //powrót na stronę główną
     public void homePageLink() {
         By homepageLinkLocator = By.id("_desktop_logo");
-        getDriver().findElement(homepageLinkLocator).click();}
+        getDriver().findElement(homepageLinkLocator).click();
+    }
+
+    //kliknięcie w button 'Proceed to checkout'
+        public void proceedToCheckoutButton() {
+            By proceedToCheckoutButtonLocator = By.xpath("//a[contains(text(),\"Proceed to checkout\")]");
+            getDriver().findElement(proceedToCheckoutButtonLocator).click();
+        }
 }
