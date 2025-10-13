867503bbe96b5acb6c037b0f7dcf8305ed528f8f
Agata Kudyk <agata.kudyk@gmail.com>
2025-10-13 14:47:25 +0200

    Lesson15 / test sklepu 'PrestaShop' / folder 'pages' - utworzenie pliku "OrderConfirmation.java"

---
+++ src/test/java/lesson15Homework/pages/OrderConfirmation.java
@@ -0,0 +1,14 @@
+package lesson15Homework.pages;
+
+import org.openqa.selenium.By;
+import static lesson15Homework.driver.DriverProvider.getDriver;
+
+
+public class OrderConfirmation {
+
+    //kliknięcie w button 'Save'
+    public void saveButtonInSaveTimeForm() {
+        By saveButtonLocator = By.xpath("//button[contains(text(),\"Save\")]");
+        getDriver().findElement(saveButtonLocator).click();
+    }
+}
