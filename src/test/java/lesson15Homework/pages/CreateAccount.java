474e03b9163c62f5609a3eb64bb0f8423d9e588e
Agata Kudyk <agata.kudyk@gmail.com>
2025-10-13 23:01:33 +0200

    Lesson15 / test sklepu 'PrestaShop' - dodanie metod 'customerPrivacyCheckbox' i 'termsAndConditionsCheckbox' w pliku "CreateAccount.java"

--- src/test/java/lesson15Homework/pages/CreateAccount.java
+++ src/test/java/lesson15Homework/pages/CreateAccount.java
@@ -1,6 +1,8 @@
 package lesson15Homework.pages;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
 import static lesson15Homework.driver.DriverProvider.getDriver;
 
 public class CreateAccount {
@@ -9,5 +11,17 @@ public class CreateAccount {
     public void saveButton() {
         By saveLocator = By.cssSelector(".form-control-submit");
         getDriver().findElement(saveLocator).click();
+    }
+
+    //kliknięcie w checkbox informacji o przetwarzaniu danych osobowych
+    public void customerPrivacyCheckbox(){
+        By policyInfoLocator = By.xpath("//input[@name=\"customer_privacy\"]");
+        getDriver().findElement(policyInfoLocator).click();
+    }
+
+    //checkbox akceptacji regulaminu i polityki prywatności
+    public void termsAndConditionsCheckbox() {
+        By privacyPolicyLocator = By.xpath("//input[@name=\"psgdpr\"]");
+       getDriver().findElement(privacyPolicyLocator).click();
     }
 }
