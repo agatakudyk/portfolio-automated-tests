ab193f7f0d1eec97f2aaf8113b661bf437797399
Agata Kudyk <agata.kudyk@gmail.com>
2025-10-13 11:39:13 +0200

    Lesson15 / test sklepu 'PrestaShop' / folder 'pages' - modyfikacja kodu w pliku "ProductTodayIsAGoodDayFramedPoster.java"

--- src/test/java/lesson15Homework/pages/ProductTodayIsAGoodDayFramedPoster.java
+++ src/test/java/lesson15Homework/pages/ProductTodayIsAGoodDayFramedPoster.java
@@ -10,11 +10,13 @@ import static lesson15Homework.driver.DriverProvider.getDriver;
 
 public class ProductTodayIsAGoodDayFramedPoster {
 
-    public void addToCartButton() {   //kliknięcie w button 'Add to cart'
+    //kliknięcie w button 'Add to cart'
+    public void addToCartButton() {
         By addTocCartLocator = By.xpath("//button[@data-button-action=\"add-to-cart\"]");
         getDriver().findElement(addTocCartLocator).click();
     }
 
+    //Popup w oknie produktu - kliknięcie w button 'Proceed to checkout'
     public void closeAddToCartPopupLocator(){
     By closeAddToCartPopupLocator = By.xpath("//a[@class=\"btn btn-primary\"]/i");
     getDriver().findElement(closeAddToCartPopupLocator).click();
