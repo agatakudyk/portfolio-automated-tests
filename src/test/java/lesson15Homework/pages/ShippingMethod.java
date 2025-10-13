be491df99d9643044a8e10d70598b59c9cf4282f
Agata Kudyk <agata.kudyk@gmail.com>
2025-10-13 13:49:03 +0200

    Lesson15 / test sklepu 'PrestaShop' / folder 'pages' - dodanie metody 'continueButton' w pliku "ShippingMethod.java"

--- src/test/java/lesson15Homework/pages/ShippingMethod.java
+++ src/test/java/lesson15Homework/pages/ShippingMethod.java
@@ -1,6 +1,13 @@
 package lesson15Homework.pages;
 
+import org.openqa.selenium.By;
+import static lesson15Homework.driver.DriverProvider.getDriver;
+
 public class ShippingMethod {
 
-
+    //kliknięcie w button 'Continue' (przejście do 'Payment')
+public void continueButton() {
+    By continueButtonShippingMethodLocator = By.xpath("//button[@name=\"confirmDeliveryOption\"]");
+    getDriver().findElement(continueButtonShippingMethodLocator).click();
+}
 }
