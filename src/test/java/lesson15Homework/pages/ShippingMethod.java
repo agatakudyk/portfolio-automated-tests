18bac30250dbbac4281c774dd730523a619b94b9
Agata Kudyk <agata.kudyk@gmail.com>
2025-10-14 20:33:06 +0200

    Lesson15 / test sklepu 'PrestaShop' - dodanie metody 'prestaShopRadioButton', 'myCarrierRadioButton' i 'commentToOrder' w pliku "ShippingMethod.java"

--- src/test/java/lesson15Homework/pages/ShippingMethod.java
+++ src/test/java/lesson15Homework/pages/ShippingMethod.java
@@ -10,4 +10,22 @@ public void continueButton() {
     By continueButtonShippingMethodLocator = By.xpath("//button[@name=\"confirmDeliveryOption\"]");
     getDriver().findElement(continueButtonShippingMethodLocator).click();
 }
+
+//wybranie formy dostawy 'My carrier'
+    public void prestaShopRadioButton() {
+    By prestaShopRadioButtonLocator = By.id("delivery_option_2");
+    getDriver().findElement(prestaShopRadioButtonLocator).click();
+    }
+
+    //wybranie formy dostawy pierwszej/nazwa ustawiana w Dockerze
+    public void myCarrierRadioButton() {
+        By myCarrierRadioButtonLocator = By.id("delivery_option_1");
+        getDriver().findElement(myCarrierRadioButtonLocator).click();
+    }
+
+    //dodanie komentarza do zamówienia
+    public void commentToOrder() {
+        By commentToOrderFieldLocator = By.id("delivery_message");
+        getDriver().findElement(commentToOrderFieldLocator).sendKeys("Proszę o zostawienie paczki pod drzwiami.");
+    }
 }
