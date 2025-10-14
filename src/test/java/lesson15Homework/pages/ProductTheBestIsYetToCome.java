dc7822cf212d49228a965765a6032fbef5766c46
Agata Kudyk <agata.kudyk@gmail.com>
2025-10-14 18:53:59 +0200

    Lesson15 / test sklepu 'PrestaShop' - dodanie metod 'sendReviewButton', 'reviewFillIn' i 'reviewTitle' w pliku "ProductTheBestIsYetToCome.java"

--- src/test/java/lesson15Homework/pages/ProductTheBestIsYetToCome.java
+++ src/test/java/lesson15Homework/pages/ProductTheBestIsYetToCome.java
@@ -1,16 +1,34 @@
 package lesson15Homework.pages;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
 import static lesson15Homework.driver.DriverProvider.getDriver;
 
 
 public class ProductTheBestIsYetToCome {
 
     //kliknięcie w button dodania opinii o produkcie
-    public void commentButton() {
-        By commentButtonLocator = By.xpath("//div[@class=\"product-comment-list-item\"]/button");
-        getDriver().findElement(commentButtonLocator).click();
+    public void productReviewButton() {
+        By reviewButtonLocator = By.xpath("//div[@class=\"product-comment-list-item\"]/button");
+        getDriver().findElement(reviewButtonLocator).click();
     }
 
+    //popup 'Write your review' - wpisanie tytułu komentarza
+    public void reviewTitle() {
+        By commentTitleLocator = By.id("comment_title");
+        getDriver().findElement(commentTitleLocator).sendKeys("Moja ocena produktu");
+    }
 
+    //popup 'Write your review' - wpisanie treści komentarza
+    public void reviewFillIn() {
+        By commentTextLocator = By.id("comment_content");
+        getDriver().findElement(commentTextLocator).sendKeys("To bardzo dobry produkt.");
+    }
+
+    //popup 'Write your review' - kliknięcie w button 'Send'
+    public void sendReviewButton() {
+        By sendButtonLocator = By.xpath("//button[@class=\"btn btn-comment btn-comment-big\"]");
+        getDriver().findElement(sendButtonLocator).click();
+    }
 }
