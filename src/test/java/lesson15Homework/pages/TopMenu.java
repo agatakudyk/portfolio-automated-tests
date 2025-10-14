cd56e2803ab5e260a810d9691898da2499ce9d8a
Agata Kudyk <agata.kudyk@gmail.com>
2025-10-14 16:54:43 +0200

    Lesson15 / test sklepu 'PrestaShop' - dodanie metody 'artPageLink' w pliku "artPageLink.java"

--- src/test/java/lesson15Homework/pages/TopMenu.java
+++ src/test/java/lesson15Homework/pages/TopMenu.java
@@ -1,6 +1,8 @@
 package lesson15Homework.pages;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
 import static lesson15Homework.driver.DriverProvider.getDriver;
 
 public class TopMenu {
@@ -11,6 +13,11 @@ public class TopMenu {
         getDriver().findElement(accessoriesPageLocator).click();
     }
 
+    //wejście na podstronę ART
+    public void artPageLink() {
+        By artPageLocator = By.id("category-9");
+        getDriver().findElement(artPageLocator).click();
+    }
 
 
 }
