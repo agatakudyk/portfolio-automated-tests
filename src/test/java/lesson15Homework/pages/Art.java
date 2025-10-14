c296cde104a0cd1039bca3450fc22ce7ce659159
Agata Kudyk <agata.kudyk@gmail.com>
2025-10-14 18:10:09 +0200

    Lesson15 / test sklepu 'PrestaShop' - dodadnie metod 'sortByNameAZ' i 'sortByPriceAsc' w pliku "Art.java"

--- src/test/java/lesson15Homework/pages/Art.java
+++ src/test/java/lesson15Homework/pages/Art.java
@@ -1,17 +1,29 @@
 package lesson15Homework.pages;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
 import static lesson15Homework.driver.DriverProvider.getDriver;
 
 public class Art {
     //kliknięcie w pole sortowania
-    public void sortBy() {
+    public void sortByButton() {
         By sortByListLocator = By.xpath("//button[@aria-label=\"Sort by selection\"]");
         getDriver().findElement(sortByListLocator).click();
     }
 
+    //posortowanie według 'Name, A to Z'
+    public void sortByNameAZ() {
+        By sortByNameAZLocator = By.xpath("//div[@class=\"dropdown-menu\"]/a[contains(text(),\"Name, A to Z\")]");
+        getDriver().findElement(sortByNameAZLocator).click();
+    }
 
-
+//posortowanie według ‘Price, low to high’
+public void sortByPriceAsc() {
+    By sortByPriceAscLocator = By.xpath("//div[@class=\"dropdown-menu\"]" +
+            "/a[contains(text(),\"Price, low to high\")]");
+    getDriver().findElement(sortByPriceAscLocator).click();
+}
 
 
 
