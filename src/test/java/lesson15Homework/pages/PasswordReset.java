791db33558a6842c2e6f2349eafe2a74de3d14b4
Agata Kudyk <agata.kudyk@gmail.com>
2025-10-14 11:52:09 +0200

    Lesson15 / test sklepu 'PrestaShop' - utworzenie pliku "PasswordReset.java"

---
+++ src/test/java/lesson15Homework/pages/PasswordReset.java
@@ -0,0 +1,22 @@
+package lesson15Homework.pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import static lesson15Homework.driver.DriverProvider.getDriver;
+
+
+public class PasswordReset {
+
+    //uzupełnienie pola 'Email address'
+    public void email() {
+        By recoveryMailLocator = By.xpath("//input[@class=\"form-control\"]");
+        getDriver().findElement(recoveryMailLocator).sendKeys("test.mail@wp.pl");
+    }
+
+    //kliknięcie w button 'Send reset link'
+    public void sendResetLink() {
+        By passwordRecoveryButtonLocator = By.id("send-reset-link");
+        getDriver().findElement(passwordRecoveryButtonLocator).click();
+    }
+}
