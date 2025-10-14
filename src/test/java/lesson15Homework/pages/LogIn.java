8f317aabbb9a698307d7c1cbb00576f8f9d6d8f1
Agata Kudyk <agata.kudyk@gmail.com>
2025-10-14 11:31:15 +0200

    Lesson15 / test sklepu 'PrestaShop' - dodanie metody 'passwordRecoveryLink' w pliku "LogIn.java"

--- src/test/java/lesson15Homework/pages/LogIn.java
+++ src/test/java/lesson15Homework/pages/LogIn.java
@@ -8,8 +8,32 @@ import static lesson15Homework.driver.DriverProvider.getDriver;
 public class LogIn {
 
     //kliknięcie w link rejestracji
-    public void signupLink () {
+    public void signupLink() {
         By signupLocator = By.cssSelector("a[data-link-action=\"display-register-form\"]");
         getDriver().findElement(signupLocator).click();
+    }
+
+    //kliknięcie w button 'Sign In'
+    public void signInButton() {
+        By loginButtonLocator = By.id("submit-login");
+        getDriver().findElement(loginButtonLocator).click();
+    }
+
+    //uzupełnienie pola 'Email / email nieistniejący w bazie
+    public void emailField() {
+        By emailInputLocator = By.id("field-email");
+        getDriver().findElement(emailInputLocator).sendKeys("blablabla@wp.pl");
+    }
+
+    // uzupełnienie pola 'Password'
+    public void passwordField() {
+        By passwordLoginLocator = By.id("field-password");
+        getDriver().findElement(passwordLoginLocator).sendKeys("blepassword");
+    }
+
+    //kliknięcie w link 'Forgot your password?'
+    public void passwordRecoveryLink() {
+        By passwordRecoveryLocator = By.xpath(" //div[@class=\"forgot-password\"]/a");
+        getDriver().findElement(passwordRecoveryLocator).click();
     }
 }
