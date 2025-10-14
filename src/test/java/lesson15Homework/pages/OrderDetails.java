f55f8d7e10e5b2583d181af765f5930baa3c6268
Agata Kudyk <agata.kudyk@gmail.com>
2025-10-14 23:58:46 +0200

    Lesson15 / test sklepu 'PrestaShop' - dodanie metod 'sendButton' i 'msgFillIn' w pliku "OrderDetails.java"

--- src/test/java/lesson15Homework/pages/OrderDetails.java
+++ src/test/java/lesson15Homework/pages/OrderDetails.java
@@ -1,7 +1,20 @@
 package lesson15Homework.pages;
 
+import org.openqa.selenium.By;
+import static lesson15Homework.driver.DriverProvider.getDriver;
+
+
 public class OrderDetails {
 
+//kliknięcie 'Send'
+    public void sendButton() {
+        By sendButtonInDetailsPageLocator = By.xpath("//*[@name=\"submitMessage\"]");
+        getDriver().findElement(sendButtonInDetailsPageLocator).click();
+    }
 
-
+    //Uzupełnienie treści wiadomości
+    public void msgFillIn() {
+        By fillInMsgTextInFormLocator = By.xpath("//textarea[@name=\"msgText\"]");
+        getDriver().findElement(fillInMsgTextInFormLocator).sendKeys("Proszę o wysyłkę możliwie najszybciej. Dziękuję.");
+    }
 }
