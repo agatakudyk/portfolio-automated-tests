d3a7f3be9fe56a1e0de96063f3582c06c7316433
Agata Kudyk <agata.kudyk@gmail.com>
2025-10-14 23:48:41 +0200

    Lesson15 / test sklepu 'PrestaShop' - dodanie metody 'detailsLink' w pliku "OrderHistoryAndDetails.java"

--- src/test/java/lesson15Homework/pages/OrderHistoryAndDetails.java
+++ src/test/java/lesson15Homework/pages/OrderHistoryAndDetails.java
@@ -1,6 +1,14 @@
 package lesson15Homework.pages;
 
+import org.openqa.selenium.By;
+import static lesson15Homework.driver.DriverProvider.getDriver;
+
+
 public class OrderHistoryAndDetails {
 
-    //
+    //kliknięcie w link 'Details'
+    public void detailsLink() {
+        By orderDetailsLinkLocator = By.xpath("//a[@data-link-action=\"view-order-details\"]");
+        getDriver().findElement(orderDetailsLinkLocator).click();
+    }
 }
