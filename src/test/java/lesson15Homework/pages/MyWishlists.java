33b15baeac6583db07e188d7bccd7eb69309c6ba
Agata Kudyk <agata.kudyk@gmail.com>
2025-10-15 22:35:40 +0200

    Lesson15 / test sklepu 'PrestaShop' - dodanie metody 'deleteButton', 'deleteWishlist', 'copyTextButton', 'shareButton' i 'renameWishlistButton' w pliku "MyWishlists.java"

--- src/test/java/lesson15Homework/pages/MyWishlists.java
+++ src/test/java/lesson15Homework/pages/MyWishlists.java
@@ -2,7 +2,6 @@ package lesson15Homework.pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
-
 import static lesson15Homework.driver.DriverProvider.getDriver;
 
 
@@ -44,5 +43,58 @@ public class MyWishlists {
     public void createWishlistButton() {
         By createWishlistButtonLocator = By.xpath("//button[contains(text(),\"Create wishlist\")]");
         getDriver().findElement(createWishlistButtonLocator).click();
+    }
+
+    //kliknięcie w trzy kropki
+    public void moreActionsButton() {
+        By moreActionLocator = By.xpath("//p[contains(text(),\"Super lista\")]" +
+                "/../div[@class=\"wishlist-list-item-right\"]/button[@class=\"wishlist-list-item-actions\"]");
+        getDriver().findElement(moreActionLocator).click();
+    }
+
+    //kliknięcie w 'Rename'
+    public void renameWishlist() {
+        By renameNewCreatedWishlistLocator = By.xpath("//p[contains(text(),\"Super lista\")]" +
+                "/..//div[@class=\"wishlist-list-item-right\"]/div[@class=\"dropdown-menu show\"]/button[contains(text(),\"Rename\")]");
+       getDriver().findElement(renameNewCreatedWishlistLocator).click();
+    }
+
+    //zmiana nazwy listy życzeń
+    public void wishlistRenameField() {
+        By changeNameOfListWishlistLocator = By.xpath("//h5[contains(text(),\"Rename wishlist\")]/../..//div/div/input");
+        WebElement changeNameOfListWishList = getDriver().findElement(changeNameOfListWishlistLocator);
+        changeNameOfListWishList.clear();
+        changeNameOfListWishList.sendKeys("Lista życzeń");
+    }
+
+    //Popup 'Rename wishlist' - kliknięcie w button 'Rename wishlist'
+    public void renameWishlistButton() {
+        By renameWishlistButtonLocator = By.xpath("//button[contains(text(),\"Rename wishlist\")]");
+        getDriver().findElement(renameWishlistButtonLocator).click();
+    }
+
+    //kliknięcie w button 'Share'
+    public void shareButton() {
+        By shareButtonWishlistLocator = By.xpath("//button[contains(text(),\"Share\")]");
+        getDriver().findElement(shareButtonWishlistLocator).click();
+    }
+
+    //kliknięcie w button 'Copy text'
+    public void copyTextButton() {
+        By copyTextButtonOfWishlistLocator = By.xpath("//button[contains(text(),\"Copy text\")]");
+        getDriver().findElement(copyTextButtonOfWishlistLocator).click();
+    }
+
+    //usunięcie listy
+    public void deleteWishlist() {
+        By deleteWishlistButtonLocator = By.xpath("//p[contains(text(),\"Lista życzeń\")]" +
+                "/../div[@class=\"wishlist-list-item-right\"]/button/i[contains(text(),\"delete\")]");
+        getDriver().findElement(deleteWishlistButtonLocator).click();
+    }
+
+    //kliknięcie w button 'Delete'
+    public void deleteButton() {
+        By deleteConfirmLocator = By.xpath("//div[@class=\"modal-footer\"]/button[contains(text(),\"Delete\")]");
+        getDriver().findElement(deleteConfirmLocator).click();
     }
 }
