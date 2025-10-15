2ee91c2c5ae54c9c05fc4e63b8d1af728220bcfa
Agata Kudyk <agata.kudyk@gmail.com>
2025-10-15 16:45:09 +0200

    Lesson15 / test sklepu 'PrestaShop' - dodanie metody 'ulubioneWishlistLink' w pliku "MyWishlists.java"

--- src/test/java/lesson15Homework/pages/MyWishlists.java
+++ src/test/java/lesson15Homework/pages/MyWishlists.java
@@ -6,9 +6,16 @@ import static lesson15Homework.driver.DriverProvider.getDriver;
 
 public class MyWishlists {
 
-    //wejście na link 'My wishlist'
+    //wejście na link listy 'My wishlist'
     public void myWishlistLink() {
         By myWishlistLinkLocator = By.xpath("//p[@class=\"wishlist-list-item-title\"]");
         getDriver().findElement(myWishlistLinkLocator).click();
+    }
+
+    //wejście na link listy 'Ulubione'
+    public void ulubioneWishlistLink() {
+        By ulubioneWishlistLinkLocator = By.xpath("//a[@class=\"wishlist-list-item-link\"]" +
+                "/p[contains(text(),\"Ulubione\")]");
+        getDriver().findElement(ulubioneWishlistLinkLocator).click();
     }
 }
