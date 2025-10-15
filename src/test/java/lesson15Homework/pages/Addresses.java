00b3d8a47f8f27e42dc2a2f5776c8a75fa415d12
Agata Kudyk <agata.kudyk@gmail.com>
2025-10-15 16:45:40 +0200

    Lesson15 / test sklepu 'PrestaShop' - aktualizacja kodu w pliku "Addresses.java"

--- src/test/java/lesson15Homework/pages/Addresses.java
+++ src/test/java/lesson15Homework/pages/Addresses.java
@@ -1,6 +1,8 @@
 package lesson15Homework.pages;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
 import static lesson15Homework.driver.DriverProvider.getDriver;
 
 
@@ -11,18 +13,5 @@ public class Addresses {
         By addreessContinuseLocator = By.xpath("//section[@id=\"checkout-addresses-step\"]" +
                 "//button[@type=\"submit\"]");
         getDriver().findElement(addreessContinuseLocator).click();
-    }
-
-    //wejście w link 'Edit'
-    public void editAddress() {
-        By editAddressesLinkLocator = By.xpath(
-                "//footer[@class=\"address-footer\"]/a[@data-link-action=\"edit-address\"]");
-        getDriver().findElement(editAddressesLinkLocator).click();
-    }
-
-    //kliknięcie w link 'Create new address'
-    public void createNewAddressLink() {
-        By createNewAddressLinkLocator = By.xpath("//a[@data-link-action=\"add-address\"]");
-        getDriver().findElement(createNewAddressLinkLocator).click();
     }
 }
