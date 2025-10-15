e9d1b842baf217394ab385ca84761bdca2164f57
Agata Kudyk <agata.kudyk@gmail.com>
2025-10-15 14:03:20 +0200

    Lesson15 / test sklepu 'PrestaShop' - dodanie metod 'editAddress', 'createNewAddressLink i 'updateAddress' w pliku "YourAddresses.java"

--- src/test/java/lesson15Homework/pages/YourAddresses.java
+++ src/test/java/lesson15Homework/pages/YourAddresses.java
@@ -12,4 +12,24 @@ public class YourAddresses {
                 "/../..//span[contains(text(),\"Delete\")]");
         getDriver().findElement(deleteNewAddressButtonLocator).click();
     }
+
+    //wejście w link 'Edit'
+    public void editAddress() {
+        By editAddressesLinkLocator = By.xpath(
+                "//footer[@class=\"address-footer\"]/a[@data-link-action=\"edit-address\"]");
+        getDriver().findElement(editAddressesLinkLocator).click();
+    }
+
+    //kliknięcie w link 'Create new address'
+    public void createNewAddressLink() {
+        By createNewAddressLinkLocator = By.xpath("//a[@data-link-action=\"add-address\"]");
+        getDriver().findElement(createNewAddressLinkLocator).click();
+    }
+
+    //kliknięcie w link 'Update'
+    public void updateAddress() {
+        By updateNewAddressButtonLocator = By.xpath("//address[text()[contains(.,\"Janowiec\")]]" +
+                "/../..//span[contains(text(),\"Update\")]");
+        getDriver().findElement(updateNewAddressButtonLocator).click();
+    }
 }
