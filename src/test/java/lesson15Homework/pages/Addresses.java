973bfa463c208f79dcd461de1ada37ef4c419bad
Agata Kudyk <agata.kudyk@gmail.com>
2025-10-15 09:41:31 +0200

    Lesson15 / test sklepu 'PrestaShop' - dodanie metody 'editAddress' w pliku "Addresses.java"

--- src/test/java/lesson15Homework/pages/Addresses.java
+++ src/test/java/lesson15Homework/pages/Addresses.java
@@ -1,6 +1,8 @@
 package lesson15Homework.pages;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
 import static lesson15Homework.driver.DriverProvider.getDriver;
 
 
@@ -11,6 +13,13 @@ public class Addresses {
         By addreessContinuseLocator = By.xpath("//section[@id=\"checkout-addresses-step\"]" +
                 "//button[@type=\"submit\"]");
         getDriver().findElement(addreessContinuseLocator).click();
+    }
+
+    //wejście w link 'Edit'
+    public void editAddress() {
+        By editAddressesLinkLocator = By.xpath(
+                "//footer[@class=\"address-footer\"]/a[@data-link-action=\"edit-address\"]");
+        getDriver().findElement(editAddressesLinkLocator).click();
     }
 
 }
