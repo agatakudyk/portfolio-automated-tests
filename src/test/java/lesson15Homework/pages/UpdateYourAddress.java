8ced6c8429bc1e5365c05de01a0ad914e545fb48
Agata Kudyk <agata.kudyk@gmail.com>
2025-10-15 13:43:32 +0200

    Lesson15 / test sklepu 'PrestaShop' - dodanie metod 'saveButton' w pliku "UpdateYourAddress.java"

---
+++ src/test/java/lesson15Homework/pages/UpdateYourAddress.java
@@ -0,0 +1,14 @@
+package lesson15Homework.pages;
+
+import org.openqa.selenium.By;
+import static lesson15Homework.driver.DriverProvider.getDriver;
+
+
+public class UpdateYourAddress {
+
+    //kliknięcie w button 'Save'
+    public void saveButton() {
+        By saveButtonLocator = By.xpath("//button[@class=\"btn btn-primary form-control-submit float-xs-right\"]");
+        getDriver().findElement(saveButtonLocator).click();
+    }
+}
