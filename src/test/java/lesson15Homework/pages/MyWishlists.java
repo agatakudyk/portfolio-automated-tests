d22df9f7a0fa19bf7041de1bd5c7d1a7546bd46f
Agata Kudyk <agata.kudyk@gmail.com>
2025-10-15 16:17:12 +0200

    Lesson15 / test sklepu 'PrestaShop' - dodanie metody 'myWishlistLink' w pliku "MyWishlists.java"

--- src/test/java/lesson15Homework/pages/MyWishlists.java
+++ src/test/java/lesson15Homework/pages/MyWishlists.java
@@ -1,5 +1,14 @@
 package lesson15Homework.pages;
 
+import org.openqa.selenium.By;
+import static lesson15Homework.driver.DriverProvider.getDriver;
+
+
 public class MyWishlists {
 
+    //wejście na link 'My wishlist'
+    public void myWishlistLink() {
+        By myWishlistLinkLocator = By.xpath("//p[@class=\"wishlist-list-item-title\"]");
+        getDriver().findElement(myWishlistLinkLocator).click();
+    }
 }
