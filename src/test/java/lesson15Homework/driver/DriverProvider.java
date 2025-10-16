6ff5eaba8693a07cac1ecf85bc5d0d5e23986a7c
Agata Kudyk <agata.kudyk@gmail.com>
2025-10-16 17:09:24 +0200

    Poprawa kodu - dodanie metody 'quitDriver' w pliku 'DriverProvider.java'

--- src/test/java/lesson15Homework/driver/DriverProvider.java
+++ src/test/java/lesson15Homework/driver/DriverProvider.java
@@ -2,30 +2,47 @@ package lesson15Homework.driver;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import java.time.Duration;
 
 
 public class DriverProvider {
 
     private static WebDriver driver;
-
-    private DriverProvider() {}
+    private static WebDriverWait wait;
+    private static String browser = "Chrome";
 
     public static WebDriver getDriver() {
 
         if (driver == null) {
-            WebDriverManager.chromedriver().setup();
+            switch (browser) {
+                case "Chrome":
+                    driver = new ChromeDriver();
+                    break;
+                case "Firefox":
+                    driver = new FirefoxDriver();
+                    break;
+                case "Edge":
+                    driver = new EdgeDriver();
+                    break;
+                default:
+                    driver = new ChromeDriver();
+            }
 
-            driver = new ChromeDriver();
-            driver.get("https://automationexercise.com");
+            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+            wait = new WebDriverWait(driver, Duration.ofSeconds(20));
         }
         return driver;
     }
+public static WebDriverWait getWaiter() {return wait;}
 
-    public static void endTest() {
+    public static void quitDriver() {
         if (driver != null) {
             driver.quit();
             driver = null;
+            wait = null;
         }
     }
 }
