e97468ccb62e8b519e3fce6de109fb99d87f4d47
Agata Kudyk <agata.kudyk@gmail.com>
2025-10-19 22:41:39 +0200

    HomeworkLesson15 - aktualizacja kodu w pliku 'Home.java'

--- src/test/java/lesson15Homework/pages/Home.java
+++ src/test/java/lesson15Homework/pages/Home.java
@@ -1,6 +1,7 @@
 package lesson15Homework.pages;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 
@@ -10,45 +11,45 @@ public class Home {
 
     //kliknięcie wishlist-button produktu 'Today is a good day Framed Poster'
     public void wishlistIconOfPosterTodayIsAGoodDay() {
-        By heartButtonOfTodayIsAGoodDayFramedPosterLocator = By.xpath(
-                "//a[contains(text(),\"Today is a good day Framed\")]" +
-                        "/../../../button[@class=\"wishlist-button-add\"]");
+        By heartButtonOfTodayIsAGoodDayFramedPosterLocator = By.xpath("//a[contains(text(),\"Today is a good day Framed\")]" + "/../../../button[@class=\"wishlist-button-add\"]");
         getDriver().findElement(heartButtonOfTodayIsAGoodDayFramedPosterLocator).click();
     }
 
+    //asercja - potwierdzenie pojawienia się popupu z komunikatem walidacyjnym
+    public boolean isMsgDisplayedThatLoginRequired() {
+    By loginRequiredMsgLocator = By.xpath("//p[contains(text(),\"You need to be logged in to " +
+            "save products in your wishlist.\")]");
+    WebElement loginRequiredMsg = getDriver().findElement(loginRequiredMsgLocator);
+    return loginRequiredMsg.isDisplayed();
+}
+
     //kliknięcie w button 'Cancel' w Popupie
     public void closeWishlistPopupOfUnregisteredUser() {
-        By cancelPopupButtonLocator = By.xpath("//a[contains(text(),\"Sign in\")]" +
-                "/../button[contains(text(),\"Cancel\")]");
+        By cancelPopupButtonLocator = By.xpath("//a[contains(text(),\"Sign in\")]" + "/../button[contains(text(),\"Cancel\")]");
         getDriver().findElement(cancelPopupButtonLocator).click();
     }
 
     //wejście na stronę produktu 'Today is a good day Framed Poster'
     public void enterProductPageOfTodayIsAGoodDayFramedPoster() {
-        By openTodayIsAGoodDayFramedPosterLocator = By.xpath("//a[@class=\"thumbnail product-thumbnail\"]" +
-                "/img[@alt=\"Today is a good day Framed poster\"]");
+        By openTodayIsAGoodDayFramedPosterLocator = By.xpath("//a[@class=\"thumbnail product-thumbnail\"]" + "/img[@alt=\"Today is a good day Framed poster\"]");
         getDriver().findElement(openTodayIsAGoodDayFramedPosterLocator).click();
     }
 
     //kliknięcie wishlist-button produktu 'Hummingbird printed t-shirt'
     public void wishlistIconOfHummingbirdPrintedTshirt() {
-        By heartButtonOfHummingbirdLocator = By.xpath(
-                "//a[contains(text(),\"Hummingbird printed t-shirt\")]" +
-                        "/../../../button[@class=\"wishlist-button-add\"]");
+        By heartButtonOfHummingbirdLocator = By.xpath("//a[contains(text(),\"Hummingbird printed t-shirt\")]" + "/../../../button[@class=\"wishlist-button-add\"]");
         getDriver().findElement(heartButtonOfHummingbirdLocator).click();
     }
 
-    //Popup 'Add to whishlist' - kliknięcie w link 'My wishlist'
+    //popup 'Add to whishlist' - kliknięcie w link 'My wishlist'
     public void myWishlistSelection() {
-        By myWishlistPopupLocator = By.xpath("//div[@class=\"modal-body\"]" +
-                "/div/ul/li[@class=\"wishlist-list-item\"]");
+        By myWishlistPopupLocator = By.xpath("//div[@class=\"modal-body\"]" + "/div/ul/li[@class=\"wishlist-list-item\"]");
         getDriver().findElement(myWishlistPopupLocator).click();
     }
 
     //kliknięcie wishlist-button produktu 'Mug The adventure begins'
     public void wishlistIconOfMugTheAdventureBegins() {
-        By heartButtonOfMugTheAdventureLocator = By.xpath("//a[contains(text(),\"Mug The adventure begins\")]" +
-                "/../../../button[@class=\"wishlist-button-add\"]");
+        By heartButtonOfMugTheAdventureLocator = By.xpath("//a[contains(text(),\"Mug The adventure begins\")]" + "/../../../button[@class=\"wishlist-button-add\"]");
         getDriver().findElement(heartButtonOfMugTheAdventureLocator).click();
     }
 
@@ -72,8 +73,15 @@ public class Home {
 
     //wybranie nowo utworzonej listy
     public void ulubioneNewWishlist() {
-        By ulubioneNewWishlistLector = By.xpath("//li[@class=\"wishlist-list-item\"]" +
-                "/p[contains(text(),\"Ulubione\")]");
+        By ulubioneNewWishlistLector = By.xpath("//li[@class=\"wishlist-list-item\"]" + "/p[contains(text(),\"Ulubione\")]");
         getDriver().findElement(ulubioneNewWishlistLector).click();
+    }
+
+    //TOAST 'Product added' - potwierdzenie pojawienia się komunikatu
+    public boolean isAddedMsgDisplayed() {
+        By productAddedToWishlistMsgLocator = By.xpath("//div[@class=\"wishlist-toast success\"]" +
+                "/p[@class=\"wishlist-toast-text\"]");
+        WebElement productAddedToWishlistMsg = getDriver().findElement(productAddedToWishlistMsgLocator);
+        return productAddedToWishlistMsg.isDisplayed();
     }
 }
