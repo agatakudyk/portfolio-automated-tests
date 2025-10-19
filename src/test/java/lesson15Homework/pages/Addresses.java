4ac4b2955d6b2dfb3cc85918ced7dfeeed5c752e
Agata Kudyk <agata.kudyk@gmail.com>
2025-10-19 22:39:16 +0200

    HomeworkLesson15 - aktualizacja kodu w pliku 'Addresses.java'

--- src/test/java/lesson15Homework/pages/Addresses.java
+++ src/test/java/lesson15Homework/pages/Addresses.java
@@ -1,6 +1,7 @@
 package lesson15Homework.pages;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
 
 import static lesson15Homework.driver.DriverProvider.getDriver;
@@ -13,5 +14,15 @@ public class Addresses {
         By addreessContinuseLocator = By.xpath("//section[@id=\"checkout-addresses-step\"]" +
                 "//button[@type=\"submit\"]");
         getDriver().findElement(addreessContinuseLocator).click();
+    }
+
+    //asercja - potwierdzenie pojawienia się komunikatu walidacyjnego
+    public String getValidationMsg() {
+        By addressesInputLocator = By.xpath("//input[@name=\"address1\"]");
+        WebElement addressesInput = getDriver().findElement(addressesInputLocator);
+
+        JavascriptExecutor js = (JavascriptExecutor) getDriver();
+        String msg = (String) js.executeScript("return arguments[0].validationMessage", addressesInput);
+        return msg;
     }
 }
