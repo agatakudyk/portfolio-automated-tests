31b8cbb1b9bab028ca90af413198494734b1ff05
Agata Kudyk <agata.kudyk@gmail.com>
2025-10-20 21:22:50 +0200

    HomeworkLesson15 - aktualizacja kodu w pliku Art.java

--- src/test/java/lesson15Homework/pages/Art.java
+++ src/test/java/lesson15Homework/pages/Art.java
@@ -20,16 +20,24 @@ public class Art {
 
     //posortowanie według 'Name, A to Z'
     public void sortByNameAZ() {
+        By sortByListLocator = By.xpath("//button[@aria-label=\"Sort by selection\"]");
+        WebElement sortBy = getDriver().findElement(sortByListLocator);
         By sortByNameAZLocator = By.xpath("//div[@class=\"dropdown-menu\"]/a[contains(text(),\"Name, A to Z\")]");
         getDriver().findElement(sortByNameAZLocator).click();
+        getWaiter().until(ExpectedConditions.stalenessOf(sortBy));
     }
 
     //posortowanie według ‘Price, low to high’
     public void sortByPriceAsc() {
+        By sortByListLocator = By.xpath("//button[@aria-label=\"Sort by selection\"]");
+        WebElement sortBy = getDriver().findElement(sortByListLocator);
+
         By sortByPriceAscLocator = By.xpath("//div[@class=\"dropdown-menu\"]" +
                 "/a[contains(text(),\"Price, low to high\")]");
         getWaiter().until(ExpectedConditions.elementToBeClickable(sortByPriceAscLocator));
         getDriver().findElement(sortByPriceAscLocator).click();
+        getWaiter().until(ExpectedConditions.stalenessOf(sortBy));
+
     }
 
     //wejście w okno produktu 'The Best Is Yet...'
@@ -40,12 +48,10 @@ public class Art {
 
     public List<WebElement> getproductsbyDescription(){
         By productsListLocator = By.xpath("//div[@class=\"product-description\"]/h2/a");
-        getWaiter().until(ExpectedConditions.elementToBeClickable(productsListLocator));
         return getDriver().findElements(productsListLocator);
     }
     public List<WebElement> getProductByPrice(){
         By productsListLocator = By.xpath("//div[@class=\"product-price-and-shipping\"]/span");
-        getWaiter().until(ExpectedConditions.elementToBeClickable(productsListLocator));
         return getDriver().findElements(productsListLocator);
     }
 
