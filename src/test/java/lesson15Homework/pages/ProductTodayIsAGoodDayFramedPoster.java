11d6515447a071b89c72455b251ec4f23ad17e65
Agata Kudyk <agata.kudyk@gmail.com>
2025-10-20 14:34:42 +0200

    HomeworkLesson15 - aktualizacja kodu w pliku ProductTodayIsAGoodDayFramedPoster.java

--- src/test/java/lesson15Homework/pages/ProductTodayIsAGoodDayFramedPoster.java
+++ src/test/java/lesson15Homework/pages/ProductTodayIsAGoodDayFramedPoster.java
@@ -1,11 +1,10 @@
 package lesson15Homework.pages;
 
-import org.junit.jupiter.api.Assertions;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 
 import static lesson15Homework.driver.DriverProvider.getDriver;
+import static lesson15Homework.driver.DriverProvider.getWaiter;
 
 
 public class ProductTodayIsAGoodDayFramedPoster {
@@ -17,17 +16,17 @@ public class ProductTodayIsAGoodDayFramedPoster {
     }
 
     //Popup w oknie produktu - kliknięcie w button 'Proceed to checkout'
-    public void closeAddToCartPopupLocator(){
-    By closeAddToCartPopupLocator = By.xpath("//a[@class=\"btn btn-primary\"]/i");
-    getDriver().findElement(closeAddToCartPopupLocator).click();
-}
+    public void closeAddToCartPopupLocator() {
+        By closeAddToCartPopupLocator = By.xpath("//a[@class=\"btn btn-primary\"]/i");
+        getDriver().findElement(closeAddToCartPopupLocator).click();
+    }
 
-//Potwierdzenie pojawienia się popupu z komunikatem potwierdzającym pomyślne dodanie
-public boolean isSuccessPopupDisplayed() {
+    //Potwierdzenie pojawienia się popupu z komunikatem potwierdzającym pomyślne dodanie
+    public boolean isSuccessPopupDisplayed() {
         By addProductPopupLocator = By.xpath("//h4[contains(text(),\"Product successfully added to " +
-            "your shopping cart\")]");
-    WebElement addProductPopup = getDriver().findElement(addProductPopupLocator);
-    return addProductPopup.isDisplayed();
-}
+                "your shopping cart\")]");
+        getWaiter().until(ExpectedConditions.elementToBeClickable(addProductPopupLocator));
+        return getDriver().findElement(addProductPopupLocator).isDisplayed();
+    }
 
 }
