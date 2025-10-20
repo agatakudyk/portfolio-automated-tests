3d61fc47c2f8f4df20bda35c5ae532bec53f289e
Agata Kudyk <agata.kudyk@gmail.com>
2025-10-20 14:29:22 +0200

    HomeworkLesson15 - aktualizacja kodu w pliku Art.java

--- src/test/java/lesson15Homework/pages/Art.java
+++ src/test/java/lesson15Homework/pages/Art.java
@@ -2,13 +2,17 @@ package lesson15Homework.pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 
 import static lesson15Homework.driver.DriverProvider.getDriver;
+import static lesson15Homework.driver.DriverProvider.getWaiter;
 
 public class Art {
+
     //kliknięcie w pole sortowania
     public void sortByButton() {
         By sortByListLocator = By.xpath("//button[@aria-label=\"Sort by selection\"]");
+        getWaiter().until(ExpectedConditions.elementToBeClickable(sortByListLocator));
         getDriver().findElement(sortByListLocator).click();
     }
 
@@ -18,14 +22,15 @@ public class Art {
         getDriver().findElement(sortByNameAZLocator).click();
     }
 
-//posortowanie według ‘Price, low to high’
-public void sortByPriceAsc() {
-    By sortByPriceAscLocator = By.xpath("//div[@class=\"dropdown-menu\"]" +
-            "/a[contains(text(),\"Price, low to high\")]");
-    getDriver().findElement(sortByPriceAscLocator).click();
-}
+    //posortowanie według ‘Price, low to high’
+    public void sortByPriceAsc() {
+        By sortByPriceAscLocator = By.xpath("//div[@class=\"dropdown-menu\"]" +
+                "/a[contains(text(),\"Price, low to high\")]");
+        getWaiter().until(ExpectedConditions.elementToBeClickable(sortByPriceAscLocator));
+        getDriver().findElement(sortByPriceAscLocator).click();
+    }
 
-//wejście w okno produktu 'The Best Is Yet...'
+    //wejście w okno produktu 'The Best Is Yet...'
     public void theBestPoster() {
         By theBestPosterLocator = By.xpath("//img[@alt=\"The best is yet to come' Framed poster\"]");
         getDriver().findElement(theBestPosterLocator).click();
