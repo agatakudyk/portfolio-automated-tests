dc74b65537c467aa1656c567a6b5ac34939c6d5b
Agata Kudyk <agata.kudyk@gmail.com>
2025-10-20 14:34:59 +0200

    HomeworkLesson15 - aktualizacja kodu w pliku ShippingMethod.java

--- src/test/java/lesson15Homework/pages/ShippingMethod.java
+++ src/test/java/lesson15Homework/pages/ShippingMethod.java
@@ -1,20 +1,22 @@
 package lesson15Homework.pages;
 
 import org.openqa.selenium.By;
+
 import static lesson15Homework.driver.DriverProvider.getDriver;
+
 
 public class ShippingMethod {
 
     //kliknięcie w button 'Continue' (przejście do 'Payment')
-public void continueButton() {
-    By continueButtonShippingMethodLocator = By.xpath("//button[@name=\"confirmDeliveryOption\"]");
-    getDriver().findElement(continueButtonShippingMethodLocator).click();
-}
+    public void continueButton() {
+        By continueButtonShippingMethodLocator = By.xpath("//button[@name=\"confirmDeliveryOption\"]");
+        getDriver().findElement(continueButtonShippingMethodLocator).click();
+    }
 
-//wybranie formy dostawy 'My carrier'
+    //wybranie formy dostawy 'My carrier'
     public void prestaShopRadioButton() {
-    By prestaShopRadioButtonLocator = By.id("delivery_option_2");
-    getDriver().findElement(prestaShopRadioButtonLocator).click();
+        By prestaShopRadioButtonLocator = By.id("delivery_option_2");
+        getDriver().findElement(prestaShopRadioButtonLocator).click();
     }
 
     //wybranie formy dostawy pierwszej/nazwa ustawiana w Dockerze
