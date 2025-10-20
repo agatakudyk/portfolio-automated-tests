cc94a3d8ec3ba0a8b723f285ff69909537925109
Agata Kudyk <agata.kudyk@gmail.com>
2025-10-20 15:57:52 +0200

    HomeworkLesson15 - aktualizacja kodu w pliku Accessories.java

--- src/test/java/lesson15Homework/pages/Accessories.java
+++ src/test/java/lesson15Homework/pages/Accessories.java
@@ -13,12 +13,18 @@ public class Accessories {
     public void ceramicCompositionFilter() {
         By ceramicCompositionFilterLocator = By.xpath("//a[contains(text(),\"Ceramic\")]");
         getDriver().findElement(ceramicCompositionFilterLocator).click();
+
+        By ceramicCompositionFilterCheckboxLocator = By.xpath("//a[contains(text(),\"Ceramic\")]/../span/input");
+        getWaiter().until(ExpectedConditions.elementToBeSelected(ceramicCompositionFilterCheckboxLocator));
     }
 
     //wybór filtra 'Available'
     public void availableFilter() {
         By availableFilterLocator = By.xpath("//a[contains(text(),\"Available\")]");
         getDriver().findElement(availableFilterLocator).click();
+
+        By availableFilterCheckboxLocator = By.xpath("//a[contains(text(),\"Available\")]/../span/input");
+        getWaiter().until(ExpectedConditions.elementToBeSelected(availableFilterCheckboxLocator));
     }
 
     //wyczyszczenie wybranych filtrów
