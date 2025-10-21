145c103232fe4d5ef3cd7240b215fc0e004ebe52
Agata Kudyk <agata.kudyk@gmail.com>
2025-10-21 12:49:21 +0200

    Aktualizacja kodu w pliku LeapYear.java

--- src/main/java/lesson5Homework/LeapYear.java
+++ src/main/java/lesson5Homework/LeapYear.java
@@ -2,20 +2,24 @@ package lesson5Homework;
 
 public class LeapYear {
 
-    public static void main(String[]args) {
+    public static void main(String[] args) {
 
         int year = 2024;
 
-        if(isLeapYear(year)){
+        if (isLeapYear(year)) {
             System.out.println(year + " - it is a leap year.");
-        } else  {
+        } else {
             System.out.println(year + "- it is not a leap year.");
         }
 
     }
 
-    public static boolean isLeapYear(int year){
-        if((year %4 == 0 && year %100 != 0) || (year%400==0)) {
+    public static boolean isLeapYear(int year) {
+        if (year < 1 || year > 9999) {
+            throw new IllegalArgumentException("Niepoprawny rok");
+        }
+
+        if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
             return true;
         }
         return false;
