cbab84c7a9205674dcc049e9b1fa1d4e5e714b14
Agata Kudyk <agata.kudyk@gmail.com>
2025-10-22 17:53:12 +0200

    HomeworkLesson16 - poprawienie kodu w pliku Addresses.java

--- src/test/java/lesson16Homework/pages/Addresses.java
+++ src/test/java/lesson16Homework/pages/Addresses.java
@@ -1,18 +1,19 @@
 package lesson16Homework.pages;
 
+import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$x;
+
 
 public class Addresses {
 
     // kliknięcie w button 'Continue' (przejście do 'Shipping Method')
-    public Addresses clickContinueButton() {
-        $x("//section[@id='checkout-addresses-step']//button[@type='submit']").click();
-        return this;
+    public void continueButton() {
+       $x("//section[@id='checkout-addresses-step']//button[@type='submit']").shouldBe(visible).click();
     }
 
-    // pobranie komunikatu walidacyjnego HTML5
-    public String getValidationMessage() {
-        return $x("//input[@name='address1']").getAttribute("validationMessage");
+    //asercja - potwierdzenie pojawienia się komunikatu walidacyjnego
+    public String getValidationMsg() {
+        return $x("//input[@name='address1']").shouldBe(visible).getAttribute("validationMessage");
     }
 }
 
