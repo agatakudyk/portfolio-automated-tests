144380a26b3239ef0d384addece59645b581e309
Agata Kudyk <agata.kudyk@gmail.com>
2025-10-23 02:55:01 +0200

    HomeworkLesson16 - poprawienie kodu w pliku ProductTodayIsAGoodDayFramedPoster.java

--- src/test/java/lesson16Homework/pages/ProductTodayIsAGoodDayFramedPoster.java
+++ src/test/java/lesson16Homework/pages/ProductTodayIsAGoodDayFramedPoster.java
@@ -7,20 +7,18 @@ import static com.codeborne.selenide.Condition.visible;
 public class ProductTodayIsAGoodDayFramedPoster {
 
     // kliknięcie w button 'Add to cart'
-    public ProductTodayIsAGoodDayFramedPoster clickAddToCartButton() {
+    public void addToCartButton() {
         $x("//button[@data-button-action='add-to-cart']").click();
-        return this;
     }
 
     // Popup w oknie produktu – kliknięcie w button 'Proceed to checkout'
-    public ProductTodayIsAGoodDayFramedPoster closeAddToCartPopup() {
+    public void closeAddToCartPopup() {
         $x("//a[@class='btn btn-primary']/i").click();
-        return this;
     }
 
     // Potwierdzenie pojawienia się popupu z komunikatem potwierdzającym dodanie produktu
-    public ProductTodayIsAGoodDayFramedPoster verifySuccessPopupDisplayed() {
-        $x("//h4[contains(text(),'Product successfully added to your shopping cart')]").shouldBe(visible);
-        return this;
+    public boolean isSuccessPopupDisplayed() {
+        return $x("//h4[contains(text(),'Product successfully added to your shopping cart')]")
+                .shouldBe(visible).isDisplayed();
     }
 }
